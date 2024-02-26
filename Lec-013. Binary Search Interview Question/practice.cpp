d2bc4d68c5d1154325d086bd5aef780be0e8774e
Ashutosh Prusty <prustyashutosh2003@gmail.com>
2024-02-26 22:23:00 +0530

    Lec-13 practice completed

---
+++ Lec-013. Binary Search Interview Question/practice.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+//first ans last occurence in an array
+int lastOcc(int arr[], int n, int key) {
+    int s = 0, e = n-1;
+
+    int mid = s + (e-s)/2;
+    int ans = -1;
+
+    while(s <= e) {
+        if(key == arr[mid]) {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if(key > arr[mid]) {
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+
+    return ans;
+}
+
+int firstOcc(int arr[], int n, int key) {
+    int s = 0, e = n-1;
+
+    int mid = s + (e-s)/2;
+    int ans = -1;
+
+    while(s <= e) {
+        if(key == arr[mid]) {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if(key > arr[mid]) {
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+
+    return ans;
+}
+
+int main() {
+    int even[11] = {1,2,3,3,3,3,3,3,3,3,5};
+
+    cout << "First Occurrence is " << firstOcc(even, 11, 3) << endl;
+    cout << "Last Occurrence is " << lastOcc(even, 11, 3) << endl;
+
+
+    return 0;
+}
